166347a6e93294ed1bd57054f249da5462610192
deepcloudlabs <deepcloudlabs@gmail.com>
2021-06-11 01:51:48 +0300

    fix fallback strategy

--- lottery-consumer-client-side-loadbalancing/src/main/java/com/example/lottery/service/LotteryService.java
+++ lottery-consumer-client-side-loadbalancing/src/main/java/com/example/lottery/service/LotteryService.java
@@ -6,15 +6,15 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.retry.annotation.Retry;
 
 @FeignClient(name = "lottery")
 public interface LotteryService {
 	@GetMapping("/lottery/api/v1/numbers")
-	@Bulkhead(name="lottery", fallbackMethod = "fallbackGetir")
+	@Retry(name="lottery", fallbackMethod = "fallbackGetir")
 	List<List<Integer>> getir(@RequestParam int column);
 	
-	default List<List<Integer>> fallbackGetir(@RequestParam int column){
+	default List<List<Integer>> fallbackGetir(@RequestParam int column,Exception e){
 		return List.of(List.of(1, 2, 3, 4, 5, 6), List.of(4, 8, 15, 16, 23, 42));
 	}
 	
