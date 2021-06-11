05332025909e49b8067657d02eed1bef08c03a92
deepcloudlabs <deepcloudlabs@gmail.com>
2021-06-11 16:35:40 +0300

    initial commit

--- study-resilience-patterns/src/main/java/com/example/service/BusinessService.java
+++ study-resilience-patterns/src/main/java/com/example/service/BusinessService.java
@@ -3,9 +3,7 @@ package com.example.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import io.github.resilience4j.bulkhead.annotation.Bulkhead;
-import io.github.resilience4j.bulkhead.annotation.Bulkhead.Type;
-import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @Service
@@ -13,9 +11,12 @@ public class BusinessService {
 	@Autowired
 	private RandomlyFailingService randomlyFailingService;
 
-	//@Retry(name = "retry1", fallbackMethod = "haveGunRetryFallback")
-	//@RateLimiter(name = "ratelimiter1", fallbackMethod = "haveGunRateLimiterFallback")
-	@Bulkhead(name = "bulkhead1", type = Type.SEMAPHORE, fallbackMethod = "haveGunBulkheadFallback")
+	@Retry(name = "retry1", fallbackMethod = "haveGunRetryFallback")
+	// @RateLimiter(name = "ratelimiter1", fallbackMethod =
+	// "haveGunRateLimiterFallback")
+	// @Bulkhead(name = "bulkhead1", type = Type.SEMAPHORE, fallbackMethod =
+	// "haveGunBulkheadFallback")
+	@CircuitBreaker(name = "circuitbreaker1", fallbackMethod = "haveGunCircuitBreakerFallback")
 	public String haveGun() {
 		return randomlyFailingService.haveFun();
 	}
@@ -23,12 +24,16 @@ public class BusinessService {
 	public String haveGunRetryFallback(Exception e) {
 		return "retry-fallback";
 	}
-	
+
 	public String haveGunRateLimiterFallback(Exception e) {
 		return "ratelimiter-fallback";
 	}
-	
+
 	public String haveGunBulkheadFallback(Exception e) {
 		return "bulkhead-fallback";
+	}
+
+	public String haveGunCircuitBreakerFallback(Exception e) {
+		return "circuitbreaker-fallback";
 	}
 }
--- study-resilience-patterns/src/main/java/com/example/service/DemoService.java
+++ study-resilience-patterns/src/main/java/com/example/service/DemoService.java
@@ -11,6 +11,9 @@ public class DemoService {
 
 	@Scheduled(fixedRate = 30_000)
 	public void demo() {
+		// com.example.service.BusinessService$$EnhancerBySpringCGLIB$$6faaecbc
+		// Dynamic Proxy -> 
+		System.err.println(businessService.getClass().getName());
 		for (var i = 1; i <= 20; ++i) {
 			var result = businessService.haveGun();
 			System.err.println(String.format("%d produces %s.", i, result));
